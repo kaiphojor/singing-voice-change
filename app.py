1fc17483d9b5840ac7ec30bf60746ad045d462e8
Atar <jihoopark7666@gmail.com>
2020-11-19 14:39:42 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask import url_for, redirect
+from flask import g
+from flask import request
+
+# flask 초기화, 이름을 포함함
+app = Flask(__name__)
+# error 보기위한 용도, debug 용도
+app.debug = True
+
+
+@app.route('/',host='index.html')
+def index():
+  return render_template('index.html')
+
+
+@app.route('/generic.html')
+def generic():
+  return render_template('generic.html')
+@app.route('/elements.html')
+def elements():
+  return render_template('elements.html')
+
+if __name__ == '__main__':
+    app.run()
