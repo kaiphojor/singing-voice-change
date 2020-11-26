70712af3159fd5f9279a4bd5fb295f2a5b816f09
Atar <jihoopark7666@gmail.com>
2020-11-26 11:14:10 +0900

    POST -> GET 수정, POST

--- app.py
+++ app.py
@@ -13,9 +13,26 @@ app.debug = True
 def index():
   return render_template('index.html')
 
-@app.route('/tts')
+@app.route('/tts',methods = ['POST','GET'])
 def tts():
-  return render_template('text-to-speech.html')
+    if request.method == 'POST':
+        text = request.form['tts-text']
+        return render_template('text-to-speech.html',singer=text)
+    elif request.method == 'GET':
+        singer = request.args.get('singer','iu')
+        return render_template('text-to-speech.html',singer=singer)
+@app.route('/create',methods = ['POST'])
+def gettext():
+    if request.method == 'POST':
+        text = request.form['tts-text']
+        return text
+
+# @app.route('/tts', methods=['POST'])
+# def my_form_post():
+#     # text = request.form['text']
+#     text = request.form.get('text',1)
+#     # processed_text = text.upper()
+#     return text
 
 @app.route('/generic')
 def generic():
